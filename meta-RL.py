95167734a7a6875d7d1ed8fa8786fe7bd3ab7855
Francesco Fraternali <francesco.fraternali@gmail.com>
2020-07-21 16:45:01 -0700

    Update meta-RL.py

--- meta-RL.py
+++ meta-RL.py
@@ -1,2 +1,2 @@
-
-# Info at https://lilianweng.github.io/lil-log/2019/06/23/meta-reinforcement-learning.html
+# Meta-Learning https://lilianweng.github.io/lil-log/2018/11/30/meta-learning.html
+# Meta-RL https://lilianweng.github.io/lil-log/2019/06/23/meta-reinforcement-learning.html
