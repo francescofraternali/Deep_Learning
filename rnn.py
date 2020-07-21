d6f1dae627d8ebe3b5be36907b0d82cf0716c06d
Francesco Fraternali <francesco.fraternali@gmail.com>
2020-07-21 16:21:00 -0700

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,1 +1,5 @@
+# Recurrent Neural Network are useful once the input data are sequential. 
+#The output of the RNN has the same size as the input since the output is feeded back into the input over and over. Learn more here https://www.youtube.com/watch?v=UNmqTiOnRfg
+#An example is stock prediction where the proce of today depends on price of yesterday. Or sequence generation, text generation, voice recognition
 
+# https://medium.com/dair-ai/building-rnns-is-fun-with-pytorch-and-google-colab-3903ea9a3a79
